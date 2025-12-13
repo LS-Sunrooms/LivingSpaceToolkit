2c9bc6092d83c9f4d977cdc9510a78fc4607c521
Chris McGlown <cmcglown@protonmail.com>
2025-12-13 01:26:30 -0500

    Made toolkit state a dataclass to store state.

---
+++ livingspacetoolkit/models/toolkit_state_model.py
@@ -0,0 +1,24 @@
+import logging
+from dataclasses import dataclass, field
+from typing import Dict
+
+from livingspacetoolkit.lib.livingspacetoolkit_enums import Scenario, RoofingType, PitchType, EndCutType, SunroomType
+
+
+@dataclass()
+class ToolkitState:
+    sunroom_type: SunroomType = SunroomType.STUDIO
+    scenario: Scenario | None = None
+    pitch: Dict[str, PitchType] | None = None
+    pitch_value: Dict[str, str] | None = None
+    overhang: str = ''
+    roofing_type: RoofingType | None = None
+    thickness: str = ''
+    end_cuts: EndCutType | None = None
+    fascia: bool = False
+    wall_heights: Dict[str, str] | None = None
+    floor_walls: Dict[str, str] = field(default_factory=lambda: {
+        'a_wall': '',
+        'b_wall': '',
+        'c_wall': '',
+    })
